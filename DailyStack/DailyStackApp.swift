075e83d9eafacbc0a1c6c8f6dbfd5f3b53db553e
Yeonju Oh <31322576+obong2@users.noreply.github.com>
2026-03-02 14:04:52 +0900

    Initial Commit

---
+++ DailyStack/DailyStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyStackApp.swift
+//  DailyStack
+//
+//  Created by yeonju.oh on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
